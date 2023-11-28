7202ec0f9556322e7b2e464441625910b82ac7ad
João Pedro <131392094+joaopedrovrg@users.noreply.github.com>
2023-11-28 17:43:06 -0300

    Update views.sql

--- sql_query/views.sql
+++ sql_query/views.sql
@@ -1,2 +1,12 @@
+-- VIEW EVENTO
+create or alter view CALENDARIO.Eventos as 
+select 
+	U.nome,
+	U.sobrenome,
+	E.titulo,
+	E.descricao, 
+	E.data
+from
+	CALENDARIO.Usuario U
+	JOIN CALENDARIO.Evento E on U.id = E.idUsuario
 
--- CRIAR UMA VIEW PARA O EVENTO
